505db91cd192b529f7428e4c38cd6cf6b6b9e5a4
jftremblay <jf_tremblay@msn.com>
2020-11-16 15:28:46 -0500

    no4 +no5 + modif

--- WeatherApp/AppConfiguration.cs
+++ WeatherApp/AppConfiguration.cs
@@ -5,25 +5,27 @@ using Microsoft.Extensions.Configuration;
 
 namespace WeatherApp
 {
-    static class AppConfiguration
+    public static class AppConfiguration
     {
-        static IConfiguration configuration;
+        private static IConfiguration configuration;
 
-        static void initConfig()
+        private static void initConfig()
         {
             var builder = new ConfigurationBuilder();
             builder.AddJsonFile("appsettings.json",optional: true,reloadOnChange: true);
             builder.AddUserSecrets<MainWindow>();
             configuration = builder.Build();
+
+          
         }
 
 
-        public static string GetValue(string key)
+        public static string GetValue(string V)
         {
             if (configuration == null)
                 initConfig();
 
-            return key;
+            return configuration[V];
         }
 
     }
--- WeatherApp/Views/MainWindow.xaml.cs
+++ WeatherApp/Views/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using OpenWeatherAPI;
+using System.Windows;
 using WeatherApp.ViewModels;
+using System.Reflection.Emit;
 
 namespace WeatherApp
 {
@@ -9,17 +11,18 @@ namespace WeatherApp
     public partial class MainWindow : Window
     {
         TemperatureViewModel vm;
-
         public MainWindow()
         {
             InitializeComponent();
 
             /// TODO : Faire les appels de configuration ici ainsi que l'initialisation
-
+            ApiHelper.InitializeClient();
+            ITemperatureService ItempService = new OpenWeatherService(AppConfiguration.GetValue("apiKey"));
             vm = new TemperatureViewModel();
+            vm.SetTemperatureService(ItempService);
 
             DataContext = vm;
-            AppConfiguration.GetValue("config");
+
         }
     }
 }
