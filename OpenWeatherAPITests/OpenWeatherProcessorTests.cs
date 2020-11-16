cfec429bf090cb4751d91807e8f6c418e10c0712
jftremblay <jf_tremblay@msn.com>
2020-11-16 12:14:39 -0500

    no3

--- OpenWeatherAPITests/OpenWeatherProcessorTests.cs
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -4,6 +4,32 @@ namespace OpenWeatherAPITests
 {
     public class OpenWeatherProcessorTests
     {
+        OpenWeatherProcessorTests _sut = new OpenWeatherProcessorTests();
+
+
+
+
+        public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+
+
+        }
+
+        public void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+
+        }
+
+        public void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+
+        }
+
+        public void GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+
+        }
+
 
     }
 }
---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace WeatherApp
+{
+    static class AppConfiguration
+    {
+        static IConfiguration configuration;
+
+        static void initConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json",optional: true,reloadOnChange: true);
+            builder.AddUserSecrets<MainWindow>();
+            configuration = builder.Build();
+        }
+
+
+        public static string GetValue(string key)
+        {
+            if (configuration == null)
+                initConfig();
+
+            return key;
+        }
+
+    }
+}
